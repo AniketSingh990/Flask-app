e88b8b73d1cfb782a41ceb0f913b7012fbe9d624
AniketSingh990 <103957257+AniketSingh990@users.noreply.github.com>
2026-06-25 21:40:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Jenkins Deployment!Aniket"
+    return "Hello, Jenkins Deployment!AniketMokama"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
